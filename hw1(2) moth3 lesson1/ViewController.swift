fc04cff94796cfa14cc476da8a4d43d216fee023
keikod1 <madragimova@icloud.com>
2022-01-07 19:15:24 +0600

    Initial Commit

---
+++ hw1(2) moth3 lesson1/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  hw1(2) moth3 lesson1
+//
+//  Created by my macbook on 6/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+    @IBOutlet weak var window: UILabel!
+    
+    @IBAction func clickOne(_ sender: Any) {
+    }
+    @IBAction func clickTwo(_ sender: Any) {
+    }
+    @IBAction func clickThree(_ sender: Any) {
+    }
+    @IBAction func clickFour(_ sender: Any) {
+    }
+    @IBAction func clickFive(_ sender: Any) {
+    }
+    @IBAction func clickSix(_ sender: Any) {
+    }
+    @IBAction func clickSeven(_ sender: Any) {
+    }
+    @IBAction func clickEight(_ sender: Any) {
+    }
+    @IBAction func clickNine(_ sender: Any) {
+    }
+}
+
